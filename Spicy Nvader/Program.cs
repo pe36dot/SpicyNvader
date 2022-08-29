1690e6f82a7202b60f5614b396a48270cdb7fdb0
pe36dot <pe36dot@eduvaud.ch>
2022-08-29 11:30:58 +0200

    Ajout des User Stories

--- Spicy Nvader/Program.cs
+++ Spicy Nvader/Program.cs
@@ -10,6 +10,11 @@ namespace Spicy_Nvader
     {
         static void Main(string[] args)
         {
+            bool soundMode = true;
+            byte gameMode = 0;
+            /*  0 = easy
+             *  1 = hard
+             */
             ConsoleKeyInfo keyPressed;
             Menu(0);
             void Menu(byte currentButton)
@@ -90,14 +95,139 @@ namespace Spicy_Nvader
             }
             void Settings()
             {
+                byte currentButton = 0;
+                Console.Clear();
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Settings");
+                ButtonUpdate();
+                for (bool exitLoop = false; !exitLoop;)
+                {
+                    keyPressed = Console.ReadKey(true);
+                    if (keyPressed.Key == ConsoleKey.UpArrow && currentButton > 0)
+                    {
+                        currentButton--;
+                        ButtonUpdate();
+                    }
+                    else if (keyPressed.Key == ConsoleKey.DownArrow && currentButton < 3)
+                    {
+                        currentButton++;
+                        ButtonUpdate();
+                    }
+                    else if (keyPressed.Key == ConsoleKey.Enter)
+                    {
+                        switch (currentButton)
+                        {
+                            case 0:
+                                if (soundMode)
+                                {
+                                    soundMode = false;
+                                }
+                                else
+                                {
+                                    soundMode = true;
+                                }
+                                break;
+                            case 1:
+                                if (gameMode == 0)
+                                {
+                                    gameMode = 1;
+                                }
+                                else
+                                {
+                                    gameMode = 0;
+                                }
+                                break;
+                            case 2:
+                                Menu(1);
+                                break;
+                        }
+                        ButtonUpdate();
+                    }
+                }
+
+                void ButtonUpdate()
+                {
+                    Console.SetCursorPosition(0, 2);
+                    Console.Write("Sound: ");
+                    if (soundMode)
+                    {
+                        Console.Write("ON ");
+                    }
+                    else
+                    {
+                        Console.Write("OFF");
+                    }
+                    Console.Write("\nDifficulty: ");
+                    switch (gameMode)
+                    {
+                        case 0:
+                            Console.Write("PADAWAN");
+                            break;
+                        case 1:
+                            Console.Write("JEDI   ");
+                            break;
+                    }
+                    Console.Write("\nReturn to menu");
+                    switch (currentButton)
+                    {
+                        case 0:
+                            Console.SetCursorPosition(0, 2);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Sound: ");
+                            if (soundMode)
+                            {
+                                Console.Write("ON ");
+                            }
+                            else
+                            {
+                                Console.Write("OFF");
+                            }
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        case 1:
+                            Console.SetCursorPosition(0, 3);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Difficulty: ");
+                            switch (gameMode)
+                            {
+                                case 0:
+                                    Console.Write("PADAWAN");
+                                    break;
+                                case 1:
+                                    Console.Write("JEDI   ");
+                                    break;
+                            }
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        case 2:
+                            Console.SetCursorPosition(0, 4);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Return to menu");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+                }
             }
             void About()
             {
                 Console.Clear();
                 Console.CursorVisible = false;
                 Console.SetCursorPosition(0, 0);
-                Console.Write("Spicy Nvader\n\nSpicy Nvader is a school project of developement in C# inspired by the iconic game known as Space Invaders.\n\n");
-                Console.SetCursorPosition(Console.WindowWidth)
+                Console.Write("About\n\nSpicy Nvader is a school project of developement in C# inspired by the iconic game known as Space Invaders.\n\n");
+                Console.SetCursorPosition(Console.WindowWidth - 5, Console.CursorTop);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Menu");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(0, 0);
+                for (bool exitLoop = false; !exitLoop;)
+                {
+                    keyPressed = Console.ReadKey(true);
+                    if (keyPressed.Key == ConsoleKey.Enter)
+                    {
+                        Menu(2);
+                    }
+                }
             }
             void Exit()
             {
